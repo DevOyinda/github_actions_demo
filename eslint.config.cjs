c373904a2009014c5dc3854b9089515e0fb96d94
DevOyinda <kudiratoyindamolad@gmail.com>
2025-03-04 15:18:05 -0500

    changed node.js file

--- eslint.config.cjs
+++ eslint.config.cjs
@@ -1,16 +1,24 @@
-module.exports = [
+modules.exports = [
   {
-    ignores: ["node_modules"], // No longer part of overrides, it goes here
+    ignores: ["node_modules"], // Ignore node_modules
     languageOptions: {
-      ecmaVersion: "latest",
-      sourceType: "module"
+      ecmaVersion: "latest", // Use the latest ECMAScript version
+      sourceType: "module", // Set the source type to module
+      globals: { structuredClone: "readonly" }, // Add structuredClone as a global
     },
     rules: {
-      "no-unused-vars": "warn",
-      "no-console": "off",
-      "semi": ["error", "always"],
-      "quotes": ["error", "double"]
-    }
-  }
+      "no-unused-vars": [
+        "warn",
+        {
+          vars: "all",
+          args: "after-used",
+          ignoreRestSiblings: false,
+        },
+      ],
+      "no-console": "warn", // Warn for console usage
+      "semi": ["error", "always"], // Enforce semicolons
+      "quotes": ["error", "double"], // Enforce double quotes
+    },
+  },
 ];
 
