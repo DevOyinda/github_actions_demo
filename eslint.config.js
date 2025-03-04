5ddae47599d41a064aaf99f5adb6d6f3c5db36ce
DevOyinda <kudiratoyindamolad@gmail.com>
2025-03-04 14:53:22 -0500

    changes

--- eslint.config.js
+++ eslint.config.js
@@ -1,18 +1,19 @@
-module.exports = [
-  {
-    ignores: ["node_modules"],
-    languageOptions: {
-      ecmaVersion: "latest",
-      sourceType: "module"
-    },
-    rules: {
-      "no-unused-vars": "warn",
-      "no-console": "warn",
-      "semi": ["error", "always"],
-      "quotes": ["error", "double"]
+module.exports = {
+  overrides: [
+    {
+      files: ["*.js"],
+      ignoredPatterns: ["node_modules"],
+      languageOptions: {
+        ecmaVersion: "latest",
+        sourceType: "module"
+      },
+      rules: {
+        "no-unused-vars": "warn",
+        "no-console": "warn",
+        "semi": ["error", "always"],
+        "quotes": ["error", "double"]
+      }
     }
-  }
-];
-
-
+  ]
+};
 
